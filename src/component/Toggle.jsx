e26a11ca91ee0ff8a3c32c6bd00cf179d547e862
Mubarak <adebowaleraji0103@gmail.com>
2025-07-15 15:34:01 +0100

    jd

--- src/component/Toggle.jsx
+++ src/component/Toggle.jsx
@@ -1,22 +1,32 @@
 import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 
 function Toggle() {
+  const [open, setOpen] = useState(false);
+  function toggleNavbar() {
+    setOpen((previousValue) => !previousValue);
+  }
+
   return (
     <div className="px-[20px] py-[10px] flex justify-between font-semibold text-[20px]">
       <div>
         <h2>Logo</h2>
       </div>
 
-      <ul className=" gap-[17px] hidden md:flex">
-        <li>Product</li>
-        <li>About us</li>
-        <li>Contact</li>
-        <li>Footer</li>
-      </ul>
+      <button onClick={toggleNavbar}>
+        <MdMenu className="text-[30px] md:hidden" />
+      </button>
 
-      <div className="text-[30px] md:hidden">
-        <MdMenu />
-      </div>
+      <nav>
+        <ul className={` hidden gap-[17px]  md:flex flex-row ${
+            open ? "block" : "hidden"
+          } md:flex`}>
+          <li>Product</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Footer</li>
+        </ul>
+      </nav>
     </div>
   );
 }
