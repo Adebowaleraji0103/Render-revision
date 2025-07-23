fc0655c8a3b8916c02af7d14587baa692a30ad2c
Mubarak <adebowaleraji0103@gmail.com>
2025-07-23 09:43:28 +0100

    s

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,25 +1,25 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules'; // ✅ import Autoplay module
 import 'swiper/css';
 
 const Slider = () => {
   return (
-    <div className='bg-red'>
-        <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      loop={true}
-      autoplay={{ delay: 3000 }}
-    >
-           <SwiperSlide><img src="images/Bag.png" alt="" /></SwiperSlide>
-           <SwiperSlide><img src="images/belt.png" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="images/canvass.png" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="images/Hat.png" alt="" /></SwiperSlide>
-
-    </Swiper>
+    <div className="bg-red-500 w-full h-auto">
+      <Swiper
+        modules={[Autoplay]} // ✅ register Autoplay module
+        spaceBetween={2}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 1000, disableOnInteraction: false }}
+        className="w-full"
+      >
+        <SwiperSlide><img src="images/Bag.png" alt="Bag" /></SwiperSlide>
+        <SwiperSlide><img src="images/image-brownie-tablet.jpg" alt="Brownie" /></SwiperSlide>
+        <SwiperSlide><img src="images/canvass.png" alt="Canvas" /></SwiperSlide>
+        <SwiperSlide><img src="images/Hat.png" alt="Hat" /></SwiperSlide>
+      </Swiper>
     </div>
   );
 };
 
 export default Slider;
-      
-
